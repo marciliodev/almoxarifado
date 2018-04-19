dd6d901b39e9e4c68f4feb71884e850ed31d5412
DerexScript <derex@outlook.com.br>
2018-04-19 17:40:21 -0300

    Correção do loop foreach
    Então, debugando rapidamente a variável $dados, notei que ela é um array bidimensional, 
    Não sei se esse realmente é essa a intenção de ser um array bidimensional, caso seja, tem que fazer um loop dentro do outro. 
    Foi o que fiz ai!

--- application/views/teste.php
+++ application/views/teste.php
@@ -2,26 +2,44 @@
 <html>
 
 <table border='1' width='1000' align='center'>
-    <tr class='header'>
-        <th id='th_desc_RSV' class='center'>Descrição do item</th>
-        <th class='center'>Quantitativo</th>
-        <th class='center'>U.F</th>
-        <th class='center'>Valor Unitário. R$</th>
-        <th class='center'>Valor Total. R$</th>
-    </tr>
-    <?php foreach ($dados as $produto): ?>
- <td class='left'><?=json_encode($produto)?></td>
- <td class='center'><?=json_encode($produto)?>></td>
- <td class='center'><?=json_encode($produto)?></td>
- <td class='center'>R$ <?=json_encode($produto)?></td>
- <td class='center'>R$ <?=json_encode($produto)?></td>
-    <?php endforeach; ?>
+	<tr class='header'>
+		<th id='th_desc_RSV' class='center'>Descrição do item</th>
+		<th class='center'>Quantitativo</th>
+		<th class='center'>U.F</th>
+		<th class='center'>Valor Unitário. R$</th>
+		<th class='center'>Valor Total. R$</th>
+	</tr>
+
+	<?php 
+	for($i=0; $i < count($dados); $i++){
+		for ($c=0; $c < count($dados[$i]); $c++){
+			?>
+			<tr>
+				<td class='left'><?php echo $dados[$i][$c]['disc_produto']; ?></td>
+				<td class='left'><?php echo $dados[$i][$c]['qt_total']; ?></td>
+				<td class='left'><?php echo $dados[$i][$c]['qt_atual']; ?></td>
+				<td class='left'>R$ <?php echo $dados[$i][$c]['vl_unitario']; ?></td>
+				<td class='left'>R$ <?php echo $dados[$i][$c]['vl_total']; ?></td>
+			</tr>
+			<?php 
+		}
+	}
+	?>
+	<!--<?php foreach ($dados[0] as $produto): ?>
+        <tr>
+            <td class='left'><?=$produto['disc_produto']?></td>
+            <td class='center'><?=$produto['qt_total']?></td>
+            <td class='center'><?=$produto['qt_atual']?></td>
+            <td class='center'>R$ <?=$produto['vl_unitario']?></td>
+            <td class='center'>R$ <?=$produto['vl_total']?></td> 
+        </tr>
+    <?php endforeach; ?> -->
     <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td class='center'><b>TOTAL</b></td>
+    	<td></td>
+    	<td></td>
+    	<td></td>
+    	<td></td>
+    	<td class='center'><b>TOTAL</b></td>
     </tr>
 </table>
 </html>
